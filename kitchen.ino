34ca75d45b63314a53fb79e237949c04e0f047a1
Alexander Drach <alexander-drach@mail.ru>
2024-02-16 15:06:32 +0600

    initial commit

---
+++ kitchen.ino
@@ -0,0 +1,96 @@
+
+#include <GyverStepper.h>
+#include "GyverButton.h"
+
+GButton stopDownSensor(2);
+GButton slowDownSpeed(3);
+
+GButton slowUpSpeed(4);
+GButton stopUpSensor(5);
+
+GButton hand(6);
+
+GButton moveUp(7);
+GButton moveDown(8);
+
+GStepper<STEPPER2WIRE> stepper(200, 11, 10);
+
+int speed = 400;
+
+bool flag = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  stepper.setRunMode(KEEP_SPEED);
+  stepper.setMaxSpeed(500);
+}
+
+void loop() {
+
+  stopDownSensor.tick();
+  slowDownSpeed.tick();
+
+  slowUpSpeed.tick();
+  stopUpSensor.tick();
+
+  hand.tick();
+
+  moveUp.tick();
+  moveDown.tick();
+
+  stepper.tick();
+
+  if (stopDownSensor.isPress()) // остановка фартука внизу ДАТЧИК - 0
+  {
+    stepper.brake();
+    Serial.println("stopDownSensor");
+    flag = false;
+  }
+
+  if (slowDownSpeed.isPress()) // замедление скорости внизу ДАТЧИК - 1
+  {
+    Serial.println("slowDownSpeed");
+  }  
+
+  if (slowUpSpeed.isPress()) { // замедление скорости вверху ДАТЧИК - 2
+    Serial.println("slowUpSpeed");
+    stepper.setSpeed(speed);
+  }
+
+  if (stopUpSensor.isPress()) { // остановка фартука вверху ДАТЧИК - 3
+    Serial.println("stopUpSensor");
+    stepper.brake();
+    flag = false;
+  }
+
+  if (hand.isPress()) {
+    Serial.println("hand sunul ruku STOP"); // проверка на руку
+    stepper.brake();
+    flag = false;
+  }
+
+  if (moveUp.isPress()) {
+    Serial.println("moveUp"); // движение фартука вверх    
+
+    if (flag) {
+      stepper.stop();
+      flag = false;
+    } else {
+      stepper.setSpeed(speed);
+      flag = true;
+    }    
+  }
+
+  if (moveDown.isPress()) { // движение фартука вниз
+    Serial.println("moveDown");
+    
+    if (flag) {
+      stepper.stop();
+      flag = false;
+    } else {
+      stepper.setSpeed(-speed);
+      flag = true;
+    };
+  }  
+}
